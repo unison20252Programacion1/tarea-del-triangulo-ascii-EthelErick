9336347138871dc2318278c2119f5da8c16dffb9
EthelErick <erickethelcordovagonzalez@gmail.com>
2025-11-21 13:45:28 -0700

    Update main.py
    actualizacion la primera equisde

--- main.py
+++ main.py
@@ -28,12 +28,13 @@ def main():
     # Intentar convertir la altura a entero
     try:
         # TODO: Convertir m_str a entero y asignarlo a m
-        pass
+        m=int(m_str)#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     except ValueError:
         # TODO: imprimir "Error: La altura debe ser un numero entero" y salir
-        pass
+        print("Error: La altura debe ser un numero entero")#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        return#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
     # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
-
+    triangulo_simetrico(m,s)
 if __name__ == "__main__":
     main()
