ff8c24f99934ea719a4dc6099e14491b2127ce5e
EthelErick <erickethelcordovagonzalez@gmail.com>
2025-11-24 14:09:04 -0700

    actualizacion se supone que esta bienmain.py

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ def main():
     # Intentar convertir la altura a entero
     try:
         # TODO: Convertir m_str a entero y asignarlo a m
-        m=int(m_str)#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        m = int(m_str)
     except ValueError:
         # TODO: imprimir "Error: La altura debe ser un numero entero" y salir
-        print("Error: La altura debe ser un numero entero")#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        return#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        print("Error: La altura debe ser un numero entero")
+        return 
+    
 
-    # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
-    triangulo_simetrico(m,s)
-if __name__ == "__main__":
+    # TODO: llamar a la función triangulo_simetrico con los parámetros m y s 
+    triangulo_simetrico(m, s)
+if _name_ == "_main_":
     main()
